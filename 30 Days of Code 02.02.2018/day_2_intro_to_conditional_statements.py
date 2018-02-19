6bca5b50dbdd93f0e930ee7f9090e00abe2f80dd
khashashin <khashashin@gmail.com>
2018-02-19 05:35:55 +0100

    day_2_intro_to_conditional_statements

---
+++ 30 Days of Code 02.02.2018/day_2_intro_to_conditional_statements.py
@@ -0,0 +1,31 @@
+# Task
+# Given an integer, n, perform the following conditional actions:
+#
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 6, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+# Complete the stub code provided in your editor to print whether or not n is weird.
+#
+# Input Format
+#
+# A single line containing a positive integer, n.
+#
+# Output Format
+#
+# Print Weird if the number is weird; otherwise, print Not Weird.
+
+#!/bin/python3
+
+import sys
+
+
+N = int(input().strip())
+if N % 2 != 0:
+    print("Weird")
+elif 2<=N<=5:
+    print("Not Weird")
+elif 6<=N<=20:
+    print("Weird")
+elif N>20:
+    print("Not Weird")
